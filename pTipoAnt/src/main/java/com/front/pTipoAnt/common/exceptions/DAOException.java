dba277c0e205b7de327a5baf6b95969dbeea448e
formacionmbm <formacionmbm@gmail.com>
2025-12-03 12:22:46 +0100

    proyecto de acceso a datos y front antiguo

---
+++ pTipoAnt/src/main/java/com/front/pTipoAnt/common/exceptions/DAOException.java
@@ -0,0 +1,29 @@
+package com.front.pTipoAnt.common.exceptions;
+
+
+/**
+ * Clase propia que recoge las excepciones del DAO
+ * @author MARIA
+ *
+ */
+@SuppressWarnings("serial")
+public class DAOException extends Exception {
+
+	TipoException tipoExcepcion;
+
+	public DAOException(TipoException tipoExcepcion) {
+		super();
+		this.tipoExcepcion = tipoExcepcion;
+	}
+
+	public TipoException getTipoExcepcion() {
+		return tipoExcepcion;
+	}
+	
+	  public String getMessage() {
+	        return this.tipoExcepcion.toString();
+	    }
+
+	
+	
+}
