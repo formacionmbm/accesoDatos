113a4d1d31b121fd5a643845d549de225304f705
Marcos <viacepeda@gmail.com>
2025-12-04 13:19:13 +0100

    Clase Region y RegionDAO

---
+++ pTipoAnt/src/main/java/com/front/pTipoAnt/data/Region.java
@@ -0,0 +1,64 @@
+package com.front.pTipoAnt.data;
+
+import java.util.Objects;
+
+/**
+ * Bean Departamento
+ * @author MARIA
+ *
+ */
+public class Region {
+	private Long id;
+	private String nombre;
+	
+	public Region() {
+		super();
+	}
+
+	public Region(Long id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Region [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Region other = (Region) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre);
+	}
+
+
+	
+}
