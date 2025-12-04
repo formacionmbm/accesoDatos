dd6e93663739b5d1e60c489c40205c43a82a4f0a
Mañanas <maanasmaanas@gmail.com>
2025-12-04 13:33:32 +0100

    RegionDAO una correción

---
+++ pTipoAnt/src/test/com/front/pTipoAnt/dao/RegionDAOTest.java
@@ -0,0 +1,14 @@
+package com.front.pTipoAnt.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegionDAOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
