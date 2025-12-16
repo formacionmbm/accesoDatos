00d9e83f5b79075820de0909e45483c94ddee1eb
Danination <moyita.dani@gmail.com>
2025-12-16 14:02:25 +0100

    jpa

---
+++ daniJPA/src/main/java/com/accesodatos/jpa/daniJPA/dao/RegionDAO.java
@@ -0,0 +1,7 @@
+package com.accesodatos.jpa.daniJPA.dao;
+
+import com.accesodatos.jpa.daniJPA.dao.interfaces.IDAO;
+
+public class RegionDAO implements IDAO<Interger, Region> {
+
+}
---
+++ daniJPA/src/main/java/com/accesodatos/jpa/daniJPA/exceptions/DAOException.java
@@ -0,0 +1,5 @@
+package com.accesodatos.jpa.daniJPA.exceptions;
+
+public class DAOException extends Exception {
+
+}
